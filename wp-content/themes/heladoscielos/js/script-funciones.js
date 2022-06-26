6e72be42f02918e1a8f1d7d8ba1fcae926b28e06
Lucia <luciamouriz91@gmail.com>
2022-06-27 00:23:35 +0200

    commit contacto y menu
    Pagina de contacto hecha!
    Menu hamburguesa para movil

--- wp-content/themes/heladoscielos/js/script-funciones.js
+++ wp-content/themes/heladoscielos/js/script-funciones.js
@@ -24,3 +24,17 @@ for (let i = 0; i < array.length; ++i) {
         
 }
 
+
+const toggleButton = document.getElementById('menu-boton-mobile')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+navWrapper.addEventListener('click',e => {
+    if(e.target.id === 'nav'){
+      navWrapper.classList.remove('show')
+      toggleButton.classList.remove('close')
+    }
+  })
